9039823c2a79c798cd23d856ed334829eb27ff3d
gjsgabriel <gjsgabriel83@gmail.com>
2022-08-21 14:49:51 -0300

    Exerrcicio Array 04

--- array/src/application/ProgramArrayExercice.java
+++ array/src/application/ProgramArrayExercice.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class ProgramArrayExercice {
@@ -8,14 +9,14 @@ public class ProgramArrayExercice {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		int[] vetorA = new int[8];
-		int[] vetorB = new int[vetorA.length];
+		int[] vetorA = new int[15];
+		double[] vetorB = new double[vetorA.length];
 		
 		for ( int i=0; i< vetorA.length; i++) {
 			System.out.println("Entre com o valor da posição:" + i);
 			vetorA[i] = scan.nextInt();
 			
-			vetorB[i] = vetorA[i] * 2;
+			vetorB[i] = Math.sqrt((vetorA[i])) ;
 		}
 		System.out.println();
 		System.out.print("Vetor A = ");
@@ -23,9 +24,12 @@ public class ProgramArrayExercice {
 			System.out.print(vetorA[i] + " ");
 		}
 		System.out.println();
+		
+		DecimalFormat df = new DecimalFormat("###,###.###");
+		
 		System.out.print("Vetor B = ");
 		for( int i=0; i < vetorB.length; i++) {
-			System.out.print(vetorB[i] + " ");
+			System.out.print(String.format("%.2f", vetorB[i])  + " ");
 		}
 		System.out.println();
 		System.out.println("--------------------------------------------");
